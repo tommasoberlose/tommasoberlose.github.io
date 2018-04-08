6ddd019630c5084ff830736c4fd8b456a9f02074
Tommaso Berlose <tommaso.berlose@gmail.com>
2018-04-08 17:30:20 +0200

    Blog update at 2018-04-08 15:30:20

--- public/ghost-sdk.js
+++ public/ghost-sdk.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var apiUrl = '//localhost:2368/ghost/api/v0.1/',
+    var apiUrl = '//localhost:2369/ghost/api/v0.1/',
         clientId,
         clientSecret,
         url,
